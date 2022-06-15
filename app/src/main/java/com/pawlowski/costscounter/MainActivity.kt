f54e9a07e7e994742f589b0ba758ed05865cc63f
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2022-06-15 16:04:30 +0200

    Added Hilt, Coroutines dependencies

--- app/src/main/java/com/pawlowski/costscounter/MainActivity.kt
+++ app/src/main/java/com/pawlowski/costscounter/MainActivity.kt
@@ -3,25 +3,28 @@ package com.pawlowski.costscounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pawlowski.costscounter.excel_related.*
+import dagger.hilt.android.AndroidEntryPoint
 import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val workBook: Workbook = XSSFWorkbook()
-        val sheetsGenerator: ExcelSheetsGenerator = ExcelSheetsGeneratorImpl(workBook, ExcelStylesUtil(workBook))
-        val items = listOf(CostItem("szafa", 15.10, 1),
-            CostItem("biurko", 10.99, 1),
-            CostItem("dlugopisy", 13.00, 1),
-            CostItem("woda", 20.17, 1)
-            )
-        sheetsGenerator.addSheetWithCostData(items, "wydatki")
-
-        val excelSaver: ExcelSaver = ExcelSaverInDocumentsImpl()
-        excelSaver.save("wydatki", workBook)
+//        val workBook: Workbook = XSSFWorkbook()
+//        val sheetsGenerator: ExcelSheetsGenerator = ExcelSheetsGeneratorImpl(workBook, ExcelStylesUtil(workBook))
+//        val items = listOf(CostItem("szafa", 15.10, 1),
+//            CostItem("biurko", 10.99, 1),
+//            CostItem("dlugopisy", 13.00, 1),
+//            CostItem("woda", 20.17, 1)
+//            )
+//        sheetsGenerator.addSheetWithCostData(items, "wydatki")
+//
+//        val excelSaver: ExcelSaver = ExcelSaverInDocumentsImpl()
+//        excelSaver.save("wydatki", workBook)
     }
 }
