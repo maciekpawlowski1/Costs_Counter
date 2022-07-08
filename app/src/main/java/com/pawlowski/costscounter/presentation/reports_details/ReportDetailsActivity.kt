a8f9f794254282c25fd57327e853356e31382e06
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2022-07-08 17:53:36 +0200

    Added ReportDetailsActivity

---
+++ app/src/main/java/com/pawlowski/costscounter/presentation/reports_details/ReportDetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.pawlowski.costscounter.presentation.reports_details
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pawlowski.costscounter.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class ReportDetailsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_report_details)
+    }
+}
